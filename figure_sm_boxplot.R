a8511e4d9b6790c543dfc30f511b9ead7155c170
Annie Chen <aschen99@unc.edu>
2024-12-05 15:47:09 -0500

    added R script to generate social media vs. stress level box plot

---
+++ figure_sm_boxplot.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(ggplot2)
+source("data.R")
+
+SM_boxplot<-ggplot(data, aes(x=Stress_Level, y=Social_Media_Usage_Hours)) + geom_boxplot() +
+  labs(title = "Stress Level vs. Social Media Use Hours",x= "Stress level", y="Social media usage hours")
+plot(SM_boxplot)
+ensure_directory("figures")
+ggsave("~/project/figures/SM_boxplot.png")
