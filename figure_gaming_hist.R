5f9aa69bb35d129cb31c4dff830f3d903f60ec95
Annie Chen <aschen99@unc.edu>
2024-12-05 15:45:41 -0500

    added R script to generate gaming histogram figure

---
+++ figure_gaming_hist.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(ggplot2)
+source("data.R")
+
+gaming_hist <-ggplot(data, aes(Gaming_Hours)) + geom_histogram(bins=24) + 
+  labs(title="Frequency of Gaming Hours", x="Gaming Hours", y="Count")
+plot(gaming_hist)
+ensure_directory("figures")
+ggsave("~/project/figures/gaming_hist.png")
